07eed25af690e37ce0133e7de8b6408e12af9eaf
AdamOsses <adamosses@users.noreply.github.com>
2022-05-29 13:22:44 +0200

    Added custom functions

--- Klasa/pierwszaklasa.py
+++ Klasa/pierwszaklasa.py
@@ -3,7 +3,16 @@
 class Paletka:
     def __init__(self, kolor):
         self.kolor_obiektu = kolor
-        print(f"Utworzyliśmy obiekt o kolorze: {self.kolor_obiektu} - ID: {id(self)}") 
+        print(f"Utworzyliśmy obiekt o kolorze: {self.kolor_obiektu} - ID: {id(self)}")
+
+    def info(self):
+        print(f"Kolor obiektu to: {self.kolor_obiektu}")
+
+    def info_ex(self, nazwa):
+        print(f"Kolor obiektu {nazwa} to: {self.kolor_obiektu}")
+
+
+
 
 def testklasy():
     paletka_a = Paletka("czerwony")
@@ -17,3 +26,8 @@ def testklasy():
     print("****************************************************")
     print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
     print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
+
+    paletka_a.info()
+    paletka_b.info()
+    paletka_a.info_ex("Paletka_a")
+    paletka_b.info_ex("Paletka_b")
