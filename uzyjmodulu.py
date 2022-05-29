54f9e04a92baef73bff614ee0a7b467480922c2d
AdamOsses <adamosses@users.noreply.github.com>
2022-05-29 10:41:16 +0200

    Added uzyjmodulu.py file

---
+++ uzyjmodulu.py
@@ -0,0 +1,12 @@
+# uzyjmodulu.py
+
+# importujemy mu modul i nadajemy mu alias
+# jezeli mamy podfoldery to podajemy tylko den ostatni a python sobie
+# da rade
+from mytoolkit import matematyczny as mat
+from mytoolkit import pomocniczy as pom
+
+print(mat.dodaj(4, 14))
+print(mat.odejmij(10, 5))
+
+pom.czesc()
