17c8183c0163aa421f0a7764429e5973f2983f60
AdamOsses <adamosses@users.noreply.github.com>
2022-05-29 13:08:40 +0200

    Added pletka_a and paletka_b

--- Klasa/pierwszaklasa.py
+++ Klasa/pierwszaklasa.py
@@ -1,10 +1,19 @@
 # pierwszaklasa.py - nauka pythona
 
 class Paletka:
-    pass
+    def __init__(self, kolor):
+        self.kolor_obiektu = kolor
+        print(f"Utworzyliśmy obiekt o kolorze: {self.kolor_obiektu} - ID: {id(self)}") 
 
 def testklasy():
-    paletka_a = Paletka()
-    print(f'Obiekt typu {type(paletka_a)} ma nast. metody:')
-    print(dir(paletka_a)) # info o wszystkich defaultowych wlasciwosciach klasy
-    # np __init__
+    paletka_a = Paletka("czerwony")
+    paletka_b = Paletka("niebieski")
+    print("****************************************************")
+    print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne właściwości i metody:")
+    print(dir(paletka_a))
+    print("****************************************************")
+    print(f"Obiekt typu {type(paletka_b)} zawiera od razu pewne właściwości i metody:")
+    print(dir(paletka_b))
+    print("****************************************************")
+    print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
+    print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
